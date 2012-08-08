99c10ffc30f4a09fa503c1e2a9be88232b32e41f
Jeremy Carbaugh <jcarbaugh@gmail.com>
2012-08-08 16:17:42 -0400

    add organization generation

---
+++ gen.py
@@ -0,0 +1,23 @@
+from flask import render_template, request
+import codecs
+import csv
+
+from web import app
+
+def unicodeit(d):
+    for k in list(d.keys()):
+        d[k] = d[k] if isinstance(d[k], unicode) else unicode(d[k], 'utf-8')
+    return d
+
+
+with app.test_request_context(''):
+
+    app.preprocess_request()
+
+    with open('data/organizations.csv') as infile:
+
+        orgs = [unicodeit(r) for r in csv.DictReader(infile)]
+        orgs = sorted(orgs, cmp=lambda x, y: cmp(x['country'], y['country']))
+
+        with codecs.open('templates/_orgs.html', 'w', 'utf-8') as outfile:
+            outfile.write(render_template('_orgs-template.html', orgs=orgs))
