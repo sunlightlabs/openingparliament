5a9d99c9594cb616c5b886777185aaad06d20898
Jeremy Carbaugh <jcarbaugh@gmail.com>
2013-04-09 11:16:01 -0400

    add about page with the timeline

--- web.py
+++ web.py
@@ -197,6 +197,11 @@ def contact():
     return render_template('contact.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.route('/declaration')
 def declaration():
     return render_template('declaration.html')
